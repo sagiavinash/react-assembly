64af4ad41bfe543a2c3a3a658b2b37e6c323cd80
Avinash Varma <s.avinash.v@uber.com>
2018-07-23 17:00:43 +0530

    simplify storeManager Api  used in App Component with just createStore

--- src/assembly/App.js
+++ src/assembly/App.js
@@ -1,31 +1,17 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import {createStore, applyMiddleware} from 'redux';
 import {Provider} from "react-redux";
+import {applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import storeManager from '../lib/storeManager';
 import Root from './Root/container';
 
 export default class App extends Component {
-  static childContextTypes = {
-    storeManager: PropTypes.object
-  };
-  getChildContext = () => ({storeManager});
   componentWillMount() {
-    const store = createStore(
-      storeManager.createRootReducer(),
-      applyMiddleware(thunk)
-    );
-
-    this.setState({store});
-
-    storeManager.refreshStore = () => {
-      this.state.store.replaceReducer(storeManager.createRootReducer());
-      console.log('this.state.store.getState()', this.state.store.getState());
-    }
+    this.setState({
+      store: storeManager.createStore({enhancer: applyMiddleware(thunk)})
+    });
   }
   render() {
-    console.log('this.state.store.getState()', this.state.store.getState());
     return (
       <Provider store={this.state.store}>
         <Root />
--- src/lib/storeManager.js
+++ src/lib/storeManager.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
+import {createStore} from 'redux';
 import reduceReducers from 'reduce-reducers';
 import {combineReducers} from 'redux';
 
 const storeManager = {
+  store: null,
   reducerMap: {},
-  register(reducerMap) {
+  registerReducer(reducerMap) {
     _.each(reducerMap, (reducer, name) => {
       if (!this.reducerMap[name]) this.reducerMap[name] = [];
 
@@ -15,6 +17,18 @@ const storeManager = {
     return combineReducers(
       _.mapValues(this.reducerMap, (reducers) => reduceReducers(...reducers))
     );
+  },
+  createStore({initialState, enhancer}) {
+    this.store = createStore(
+      this.createRootReducer(),
+      ..._.compact([initialState, enhancer]),
+    );
+
+    return this.store;
+  },
+  refreshStore() {
+    this.store.replaceReducer(this.createRootReducer());
+    console.log('this.store.getState()', this.store.getState());
   }
 };
 
